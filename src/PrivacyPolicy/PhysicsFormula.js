fdff1cc6ba2cd9599e537da20e4cade9493d70fc
zoro4rk <58879533+thuyetngocluong@users.noreply.github.com>
2023-01-04 02:48:02 +0700

    Update PhysicsFormula.js

--- src/PrivacyPolicy/PhysicsFormula.js
+++ src/PrivacyPolicy/PhysicsFormula.js
@@ -1,1 +1,31 @@
 
+import React from "react";
+import "../css/PrivacyPolicy.css"
+
+function PhysicsFormulaApp() {
+    return (
+        <div>
+            <br/>
+            <br/>
+            <span style={{ fontWeight: 'bold', fontSize: '40px' }}>Điều khoản sử dụng</span>
+            <br/><br/><br/>
+
+            <p style={{ textAlign: 'left', fontWeight: 'bold', fontSize: '20px', paddingLeft: '20px'}}>1. GIỚI THIỆU</p>
+
+            <p style={{textAlign: 'left' , paddingLeft: '20px'}}>Ứng dụng PhysicsFormula là ứng dụng tổng hợp các công thức nhanh phục vụ học sinh thi THPT quốc gia .</p>
+        
+            <p style={{ textAlign: 'left', fontWeight: 'bold', fontSize: '20px', paddingLeft: '20px'}}>2. PhysicsFormula sẽ thu thập những dữ liệu gì?</p>
+
+            <p style={{textAlign: 'left', paddingLeft: '20px'}}>Ứng dụng hoàn toàn không thu thập dữ liệu cá nhân người dùng.</p>
+
+            <p style={{ textAlign: 'left', fontWeight: 'bold', fontSize: '20px' , paddingLeft: '20px'}}>3. THẮC MẮC, QUAN NGẠI HOẶC KHIẾU NẠI?</p>
+
+            <p style={{textAlign: 'left', paddingLeft: '20px'}}>Liên hệ với tôi qua email: <a href="mailto:thuyetngocluong.working@gmail.com">thuyetngocluong.working@gmail.com</a></p>
+
+            <p style={{ textAlign: 'left', fontWeight: 'bold', fontSize: '15px' , paddingLeft: '20px'}}>Lần cập nhật cuối 04/01/2023</p>
+        </div>
+       
+    );
+}
+
+export default DictionaryApp;
