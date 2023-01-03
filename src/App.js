a6b9c67a32b2d0c71df03d4e45f8f6a9d1c780f1
zoro4rk <58879533+thuyetngocluong@users.noreply.github.com>
2023-01-04 02:53:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 
 import DiaryApp from './PrivacyPolicy/DiaryApp.js';
 import DictionaryApp from './PrivacyPolicy/DictionaryApp';
-import PhysicsFormulaApp from './PrivacyPolicy/PhysicsFormula';
+import PhysicsFormulaApp from './PrivacyPolicy/PhysicsFormula.js';
 
 
 function App() {
